d94f15c1414fcdade2a0e88caf4df8bf7c93b28a
thanatat.bunn <thanatat.bunn@bumail.net>
2024-09-24 16:42:41 +0700

    peach monkey
    the end

---
+++ Assets/Scripts/Lab/Crocodlie.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodlie : Enemy
+{
+   public override void Behavior()
+
+        {
+            Debug.Log("Crocodile.Behavior");
+        }
+    
+}
---
+++ Assets/Scripts/Lab/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public void Shoot()
+    {
+
+    }
+
+    public void OnHitWithEnemy()
+    {
+
+    }
+}
