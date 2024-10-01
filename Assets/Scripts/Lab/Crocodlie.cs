910d3d88347efc7f026d1d9f6f6b6a844c5ab374
peach4812 <thanatat.bunn@bumail.net>
2024-10-01 16:55:49 +0700

    coco shoot

--- Assets/Scripts/Lab/Crocodlie.cs
+++ Assets/Scripts/Lab/Crocodlie.cs
@@ -1,13 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Crocodlie : Enemy
 {
-   public override void Behavior()
 
+    [SerializeField] private float attackRange;
+    public Player player;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletSpawnTime;
+    [SerializeField] private float bulletTimer;
+
+    private void Update()
+    {
+        bulletTimer -= Time.deltaTime;
+
+        Behavior();
+        if(bulletTimer < 0)
         {
-            Debug.Log("Crocodile.Behavior");
+            bulletTimer = bulletSpawnTime;
         }
-    
+    }
+
+
+    public override void Behavior()
+
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (bulletTimer <= 0)
+        {
+
+            Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+        }
+
+    }
 }
